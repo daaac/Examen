decccfe30f3b8fcb4083d57cfeab13b57769d706
Marc Capdevila <pro.marc.capdevila@gmail.com>
2012-11-15 16:08:18 +0100

    Primera Version

---
+++ gitexamen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gitexamen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Hola mundo");
+        }
+    }
+}
